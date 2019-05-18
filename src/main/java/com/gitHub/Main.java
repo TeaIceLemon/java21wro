09bca34546807b6cfd698b36596dca80e63f2d7f
TeaIceLemon <45081706+TeaIceLemon@users.noreply.github.com>
2019-05-18 15:11:46 +0200

    Create enum Nominal (#2)
    * Created enum,
    * Create Main class.

---
+++ src/main/java/com/gitHub/Main.java
@@ -0,0 +1,9 @@
+package com.gitHub;
+
+public class Main {
+    public static void main(String[] args) {
+        for (Nominal value : Nominal.values()) {
+            System.out.println(value);
+        }
+    }
+}
